f3942be35321d137f82693add4378dbae55cef8b
KevinEjiofor <ejioforkelvin@gmail.com>
2024-06-14 12:48:57 +0100

    new update

--- app/Http/Controllers/WalletController.php
+++ app/Http/Controllers/WalletController.php
@@ -1,12 +1,14 @@
 <?php
 
-;
+
+namespace App\Http\Controllers;
 
 use App\Models\User;
 use App\Models\Wallet;
 use App\Models\Transaction;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
 
 class WalletController extends Controller
 {
--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WalletController;
 
 Route::get('/users', [WalletController::class, 'getUsers']);
-Route::get('/wallets', [WalletController::class, 'getWallets']);
-Route::get('/wallet/{wallet_id}', [WalletController::class, 'getWalletDetails']);
-Route::post('/wallet/transfer', [WalletController::class, 'transferMoney']);
+Route::get('/wallets', [WalletControler::class, 'getWallets']);
+Route::get('/wallet/{wallet_id}', [WalletControler::class, 'getWalletDetails']);
+Route::post('/wallet/transfer', [WalletControler::class, 'transferMoney']);
 
 
 Route::get('/', function () {
